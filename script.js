e3670a6904a1a3918c77434b1807860b07638c21
Evgen <yevgenchikk@mail.ru>
2023-08-10 22:18:04 +0300

    feat: add mobile layout

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const GIFTS = {
+  vidget: '30 виджетов',
+  armocrm: 'Месяц аmoCRM',
+};
+
+const giftVidgetBlock = document.querySelector('.gift-vidget');
+const giftArmoCrmBlock = document.querySelector('.gift-armocrm');
+
+if (window.innerWidth < 478) {
+  giftVidgetBlock.textContent = GIFTS.vidget;
+  giftArmoCrmBlock.textContent = GIFTS.armocrm;
+}
