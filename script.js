ee853f187c4102ce07d8c73bf085c5a0eb761840
Evgen <yevgenchikk@mail.ru>
2023-08-11 11:40:41 +0300

    balls in separete file svg

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ const GIFTS = {
 const giftVidgetBlock = document.querySelector('.gift-vidget');
 const giftArmoCrmBlock = document.querySelector('.gift-armocrm');
 
+const redBall = document.querySelector('.red-ball__img');
+const purpleBall = document.querySelector('.purple-ball__img');
+
 if (window.innerWidth < 478) {
   giftVidgetBlock.textContent = GIFTS.vidget;
   giftArmoCrmBlock.textContent = GIFTS.armocrm;
+
+  redBall.setAttribute('src', './icons/red-ball-mob.svg');
+  purpleBall.setAttribute('src', './icons/purple-ball-mob.svg');
 }
