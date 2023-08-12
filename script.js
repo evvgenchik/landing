a3a6d0707d98eaea201844e18a50f4fb0f7a9ce7
Evgen <yevgenchikk@mail.ru>
2023-08-12 22:33:15 +0300

    refactor

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const GIFTS = {
 const giftVidgetBlock = document.querySelector('.gift-vidget');
 const giftArmoCrmBlock = document.querySelector('.gift-armocrm');
 
-const redBall = document.querySelector('.red-ball__img');
-const purpleBall = document.querySelector('.purple-ball__img');
-
 if (window.innerWidth < 768) {
   giftVidgetBlock.textContent = GIFTS.vidget;
   giftArmoCrmBlock.textContent = GIFTS.armocrm;
